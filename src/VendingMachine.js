dd3ec04cc0c5068e0ef8b1e3c73bdbf7f2a9d717
Carson Barry <carsonbarry@MacBook-Pro.local>
2023-09-07 17:41:37 -0500

    styling, creating components

---
+++ src/VendingMachine.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { link } from "react-router-dom";
+import machinePicture from "./vending-machine.jpg"; //Image AI generated using https://www.fotor.com/
+import "./VendingMachine.css";
+import VendingMachineSelect from "./VendingMachineSelect"
+
+const VendingMachine = () => {
+
+    return (
+        <div className="VendingMachine">
+            <img className="VendingMachine-img" src={machinePicture} alt="Vending machine"></img>
+            <VendingMachineSelect />
+        </div>
+    );
+}
+
+export default VendingMachine;
---
+++ src/VendingMachineSelect.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./VendingMachineSelect.css"
+
+const VendingMachineSelect = () => {
+    return (
+        <div className="VendingMachineSelect">
+            <h1 className="VendingMachineSelect-title">Vending Machine</h1>
+        </div>
+    )
+}
+
+export default VendingMachineSelect;
